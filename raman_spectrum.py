d0c674ba44022214b029a5b2ff0470a1816d7930
Louis-Bernard St-Cyr <lbstc@ulaval.ca>
2022-03-30 09:35:25 -0400

    prominence

--- raman_spectrum.py
+++ raman_spectrum.py
@@ -5,10 +5,11 @@ from scipy.signal import find_peaks
 
 
 class RamanSpectrum:
-    def __init__(self, fich):
+    def __init__(self, fich, promi):
         self.data = np.genfromtxt(fich, delimiter=',')
         self.raman = self.data[:, 1]
         self.counts = self.data[:, 2]
+        self.promi = int(promi)
         self.peaks = self.getPeaks()
         self.name = fich
 
@@ -21,8 +22,26 @@ class RamanSpectrum:
         plt.legend()
         plt.xlabel('Raman shift')
         plt.ylabel('Counts')
+    def graph_zero(self, peaks=False):
+        liste = list(zip(self.raman, self.counts))
+        raman = []
+        counts = []
+        for x in liste:
+            if x[0] >= 0:
+                raman.append(x[0])
+                counts.append(x[1])
+        plt.plot(raman, counts, label=f'{self.name}')
+        if peaks:
+            for x in self.peaks:
+                plt.plot(x[0], x[1], 'o', color='red')
+                plt.text(x[0], x[1] + 0.5, '({}, {})'.format(x[0], x[1]))
+                plt.legend()
+                plt.xlabel('Raman shift')
+                plt.ylabel('Counts')
 
-    def getPeaks(self, promi=1800):
+
+    def getPeaks(self):
+        promi = self.promi
         ens_ini = list(zip(self.data[:, 1], self.data[:, 2]))
         raman = []
         counts = []
@@ -38,8 +57,10 @@ class RamanSpectrum:
             points_maxi.append(ens_fin[int(x)])
         return points_maxi
 
-olive = RamanSpectrum("huile_olive_100s.txt")
+olive = RamanSpectrum("huile_olive_100s.txt", 1800)
 olive.graph(peaks=True)
 #olive_1 = RamanSpectrum("huile_olive1.txt")
 #olive_1.graph(peaks=True)
+plt.show()
+olive.graph_zero(peaks=True)
 plt.show()
