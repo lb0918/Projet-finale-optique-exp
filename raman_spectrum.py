6ffcea6eb5b12ea4f602ffeba99417dcb57dc6ed
embar31 <baril.emile@gmail.com>
2022-03-30 08:35:20 -0400

    Classe raman_spectrum.py

---
+++ raman_spectrum.py
@@ -0,0 +1,45 @@
+"Fichier qui implémente la classe d'objets raman_spectum qui sert à analyser les spectres raman"
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
+
+
+class RamanSpectrum:
+    def __init__(self, fich):
+        self.data = np.genfromtxt(fich, delimiter=',')
+        self.raman = self.data[:, 1]
+        self.counts = self.data[:, 2]
+        self.peaks = self.getPeaks()
+        self.name = fich
+
+    def graph(self, peaks=False):
+        plt.plot(self.raman, self.counts, label=f'{self.name}')
+        if peaks:
+            for x in self.peaks:
+                plt.plot(x[0], x[1], 'o', color='red')
+                plt.text(x[0], x[1] + 0.5, '({}, {})'.format(x[0], x[1]))
+        plt.legend()
+        plt.xlabel('Raman shift')
+        plt.ylabel('Counts')
+
+    def getPeaks(self, promi=1800):
+        ens_ini = list(zip(self.data[:, 1], self.data[:, 2]))
+        raman = []
+        counts = []
+        for x in ens_ini:
+            if x[0] >= 0:
+                raman.append(x[0])
+                counts.append(x[1])
+        raman, counts = np.array(raman), np.array(counts)
+        ens_fin = list(zip(raman, counts))
+        points_maxi = []
+        b = find_peaks(counts, prominence=promi)
+        for x in b[0]:
+            points_maxi.append(ens_fin[int(x)])
+        return points_maxi
+
+olive = RamanSpectrum("huile_olive_100s.txt")
+olive.graph(peaks=True)
+#olive_1 = RamanSpectrum("huile_olive1.txt")
+#olive_1.graph(peaks=True)
+plt.show()
